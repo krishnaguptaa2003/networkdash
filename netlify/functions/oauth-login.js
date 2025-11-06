693f04f60ca7827adba6aaebbf19b0859591d082
krishna Gupta <krishnaguptaa2003@gmail.com>
2025-11-06 13:52:26 +0530

    updated OAuth-login.js

--- netlify/functions/oauth-login.js
+++ netlify/functions/oauth-login.js
@@ -1,44 +1,49 @@
 /* eslint-disable no-undef */
-// This function builds the correct URL and redirects the user
-// to the social provider's login page.
+const crypto = require('crypto');
 
+// This function just builds the correct URL and redirects the user
 exports.handler = async (event) => {
-  const { provider } = event.queryStringParameters;
-  const redirectUri = `${process.env.URL}/.netlify/functions/oauth-handler`;
+  const provider = event.queryStringParameters.provider;
+  let authUrl;
 
-  let authUrl = '';
+  // Use the live URL or localhost
+  const rootUrl = process.env.URL || 'http://localhost:8888';
+  
+  // This is the URL our backend handler will live at
+  const redirectUri = `${rootUrl}/.netlify/functions/oauth-handler?provider=${provider}`;
 
-  switch (provider) {
-    case 'google': {
-      const scope = [
-        'https://www.googleapis.com/auth/userinfo.email',
-        'https://www.googleapis.com/auth/userinfo.profile'
-      ].join(' ');
-      
-      authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}?provider=google&response_type=code&scope=${scope}&prompt=select_account`;
-      break;
-    }
-    case 'github': {
-      const scope = 'read:user,user:email';
-      authUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${redirectUri}?provider=github&scope=${scope}`;
-      break;
-    }
-    case 'twitter': {
-      // Twitter OAuth 2.0 uses a different flow (PKCE) which is more complex
-      // For now, let's log a placeholder
-      console.warn('Twitter login is not yet implemented');
-      // We'll just redirect back to login for now
-      authUrl = `${process.env.FRONTEND_URL}/login?error=twitter_not_implemented`;
-      break;
-    }
-    default:
-      return {
-        statusCode: 400,
-        body: 'Invalid provider',
-      };
+  if (provider === 'google') {
+    const scope = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile'
+    ].join(' ');
+
+    authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&prompt=select_account`;
+  
+  } else if (provider === 'github') {
+    
+    // --- THIS IS THE FIX ---
+    // It's a space ' ' not a comma ','
+    const scope = 'read:user user:email';
+    // --- END OF FIX ---
+
+    authUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scope}`;
+  
+  } else if (provider === 'twitter') {
+    // Twitter is much more complex (PKCE flow)
+    // We will build this in the next step.
+    return {
+      statusCode: 302,
+      headers: { Location: `${process.env.FRONTEND_URL}/login?error=twitter_not_implemented` },
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: 'Invalid provider',
+    };
   }
 
-  // Redirect the user
+  // Redirect the user to the social login page
   return {
     statusCode: 302,
     headers: {
@@ -46,3 +51,4 @@ exports.handler = async (event) => {
     },
   };
 };
+
